7f078fd2bc27273f4b92fcdc121f440996a82a4b
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-12-16 21:16:38 +0300

    Add Network methods Add custom cell

--- RickAndMortyTest/Network/Views/EpisodeCustomCell.swift
+++ RickAndMortyTest/Network/Views/EpisodeCustomCell.swift
@@ -6,25 +6,34 @@
 //
 
 import UIKit
-
 class EpisodeCustomCell: UICollectionViewCell {
     let titleLabel: UILabel = {
         let label = UILabel()
-        // Настройте свойства метки
         return label
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        // Добавьте метку к ячейке
-        addSubview(titleLabel)
         
-        // Настройте размещение и размер метки
+        addSubview(titleLabel)
+        addSubview(imageView)
+        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
